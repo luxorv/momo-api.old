43276e105fd7e19a8ad42803f734462ca233f427
Rapito <robert.abp@hotmail.com>
2014-08-10 15:44:19 +0000

    Fixing Github settings for vagrant file (Robert)

--- vagrant-setup.sh
+++ vagrant-setup.sh
@@ -28,7 +28,7 @@ vagrant ssh -c "echo 'gem: --no-ri --no-rdoc' > ~/.gemrc"
 #configure git
 vagrant ssh -c "git config --global color.ui true"
 vagrant ssh -c "git config --global user.name Rapito"
-vagrant ssh -c "git config --global user.email robert.abp@hotmail.com"
+vagrant ssh -c "git config --global user.email robert.apb@hotmail.com"
 vagrant ssh -c "ssh-keygen -t rsa -C robert.apb@hotmail.com"
 echo "Copy your public key and paste in github!"
 vagrant ssh -c "cat ~/.ssh/id_rsa.pub"
