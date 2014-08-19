50cb0609e6c78ece604ae15790824597bcf4d185
rapito <robert.apb@hotmail.com>
2014-08-19 18:32:06 +0000

    Created Module

---
+++ app/services/pusher.rb
@@ -0,0 +1,17 @@
+=begin
+
+  Pusher class: this class is used as a database transactions-handler abstraction module.
+  This class handles User Actions and Features that requires to push a transaction to the DB.
+
+  Uses: x class.
+
+  - This class will be injected in some method for the controllers that require it.
+  - Redirect required GET/PUT/POST/DELETE methods controllers to Pusher Module
+
+=end
+
+class Pusher
+
+
+
+end
