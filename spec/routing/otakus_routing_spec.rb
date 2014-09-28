72fc0fff3cfbcea5c041d0f00f261e629d77ac22
rapito <robert.apb@hotmail.com>
2014-09-28 07:48:24 +0000

    Removing rspec warnings and updating routes specs

--- spec/routing/otakus_routing_spec.rb
+++ spec/routing/otakus_routing_spec.rb
@@ -1,24 +1,24 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe OtakusController, :type => :routing do
-  describe "routing" do
+  describe 'routing' do
 
-    it "routes to #index" do
-      expect(:get => "/otakus").to route_to("otakus#index")
+    it 'routes to #index' do
+      expect(:get => '/otakus').to route_to('otakus#index')
     end
 
-    it "routes to #show" do
-      expect(:get => "/otakus/1").to route_to("otakus#show", :id => "1")
+    it 'routes to #show' do
+      expect(:get => '/otakus/1').to route_to('otakus#show', :id => '1')
     end
 
-    it "routes to lists" do
-      expect(:post => "/otakus/1/watch").to route_to("otakus#add_to_watch_list", :id => "1")
-      expect(:post => "/otakus/1/watched").to route_to("otakus#add_to_watched_list", :id => "1")
-      expect(:post => "/otakus/1/watching").to route_to("otakus#add_to_watching_list", :id => "1")
+    it 'routes to lists' do
+      expect(:post => '/otakus/1/watch/2').to route_to('otakus#add_to_watch_list', :id => '1',:anime_id => '2')
+      expect(:post => '/otakus/1/watched/2').to route_to('otakus#add_to_watched_list', :id => '1',:anime_id => '2')
+      expect(:post => '/otakus/1/watching/2').to route_to('otakus#add_to_watching_list', :id => '1',:anime_id => '2')
 
-      expect(:delete => "/otakus/1/watch").to route_to("otakus#remove_from_watch_list", :id => "1")
-      expect(:delete => "/otakus/1/watched").to route_to("otakus#remove_from_watched_list", :id => "1")
-      expect(:delete => "/otakus/1/watching").to route_to("otakus#remove_from_watching_list", :id => "1")
+      expect(:delete => '/otakus/1/watch/2').to route_to('otakus#remove_from_watch_list', :id => '1',:anime_id => '2')
+      expect(:delete => '/otakus/1/watched/2').to route_to('otakus#remove_from_watched_list', :id => '1',:anime_id => '2')
+      expect(:delete => '/otakus/1/watching/2').to route_to('otakus#remove_from_watching_list', :id => '1',:anime_id => '2')
     end
 
   end
